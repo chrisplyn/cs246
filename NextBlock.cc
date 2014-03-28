5ef2a17e1f18b5bd61e82e23d88017a365c5bf72
chrisplyn <y652liu@uwaterloo.ca>
2014-03-28 17:07:25 -0400

    Update NextBlock.cc

--- NextBlock.cc
+++ NextBlock.cc
@@ -28,11 +28,8 @@ string NextBlock::getNonRandomType(){
 	return type;
 }
 
-<<<<<<< HEAD
+
 // return true if input block stream is exhausted
-=======
-// check whether input block stream is exhausted
->>>>>>> 808bdc4bc228f11c24082ca3c9d6f803c6c5e304
 bool NextBlock::noRandomType(){
 	return pImpl->failed;
 }
