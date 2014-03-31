c65e41645cbbfeab123cfc2d395140eb398f263e
elkenwang <wangxiangkang@gmail.com>
2014-03-30 22:20:35 -0400

    change

--- tblock.cc
+++ tblock.cc
@@ -70,14 +70,14 @@ void TBlock::rotateClockwise(Board *board) {
     Cell **grid = board->getGrid();
     
     if (direction()==0) {
-        cells[0]->Swap(&grid[cells[0]->getX()][cells[0]->getY()+2]);
-        cells[1]->Swap(&grid[cells[1]->getX()-1][cells[1]->getY()+1]);
-        cells[2]->Swap(&grid[cells[2]->getX()-2][cells[2]->getY()]);
+        cells[0]->Swap(&grid[cells[0]->getX()-2][cells[0]->getY()]);
+        cells[1]->Swap(&grid[cells[1]->getX()-1][cells[1]->getY()-1]);
+        cells[2]->Swap(&grid[cells[2]->getX()][cells[2]->getY()-2]);
     } else if (direction()==1) {
-        cells[3]->Swap(&grid[cells[3]->getX()][cells[3]->getY()-1]);
-        cells[0]->Swap(&grid[cells[0]->getX()+2][cells[0]->getY()-1]);
-        cells[1]->Swap(&grid[cells[1]->getX()+1][cells[1]->getY()]);
-        cells[2]->Swap(&grid[cells[2]->getX()][cells[2]->getY()+1]);
+        cells[3]->Swap(&grid[cells[3]->getX()+1][cells[3]->getY()]);
+        cells[0]->Swap(&grid[cells[0]->getX()+1][cells[0]->getY()+2]);
+        cells[1]->Swap(&grid[cells[1]->getX()][cells[1]->getY()+1]);
+        cells[2]->Swap(&grid[cells[2]->getX()-1][cells[2]->getY()]);
     } else if (direction()==2) {
         cells[2]->Swap(&grid[cells[2]->getX()+1][cells[2]->getY()+1]);
         cells[3]->Swap(&grid[cells[3]->getX()-1][cells[3]->getY()+1]);
@@ -352,16 +352,16 @@ bool TBlock::canRotateAnticlockwise(Board *board) {
     int originY = cells[1]->getY();
 
     if (direction()==0) {
-        if (grid[originX][originY+1].isOn()) return false;
-        if (grid[originX+1][originY+2].isOn()) return false;
-    } else if (direction()==1) {
-        if (grid[originX+1][originY-2].isOn()) return false;
-        if (grid[originX+2][originY-2].isOn()) return false;
-    } else if (direction()==2) {
+        if (grid[originX-1][originY].isOn()) return false;
         if (grid[originX-2][originY+1].isOn()) return false;
-        if (grid[originX-2][originY-1].isOn()) return false;
+    } else if (direction()==1) {
+        if (grid[originX+2][originY+1].isOn()) return false;
+        if (grid[originX+2][originY+2].isOn()) return false;
+    } else if (direction()==2) {
+        if (grid[originX-1][originY-2].isOn()) return false;
+        if (grid[originX+1][originY-2].isOn()) return false;
     } else {
-        if (grid[originX+1][originY+1].isOn()) return false;
+        if (grid[originX-2][originY].isOn()) return false;
     }
     return true;
 }
@@ -374,13 +374,13 @@ void TBlock::moveLeft(Board *board){
         for (int i =0; i<4; ++i) {
             int coorX = cells[i]->getX();
             int coorY = cells[i]->getY();
-            cells[i]->Swap(&grid[coorX-1][coorY]);
+            cells[i]->Swap(&grid[coorX][coorY-1]);
         }
     } else {
         for (int i =3; i>=0; --i) {
             int coorX = cells[i]->getX();
             int coorY = cells[i]->getY();
-            cells[i]->Swap(&grid[coorX-1][coorY]);
+            cells[i]->Swap(&grid[coorX][coorY-1]);
         }
     }
 }
@@ -393,13 +393,13 @@ void TBlock::moveRight(Board *board) {
         for (int i =0; i<4; ++i) {
             int coorX = cells[i]->getX();
             int coorY = cells[i]->getY();
-            cells[i]->Swap(&grid[coorX+1][coorY]);
+            cells[i]->Swap(&grid[coorX][coorY+1]);
         }
     } else {
         for (int i =3; i>=0; --i) {
             int coorX = cells[i]->getX();
             int coorY = cells[i]->getY();
-            cells[i]->Swap(&grid[coorX+1][coorY]);
+            cells[i]->Swap(&grid[coorX][coorY+1]);
         }
     }
 }
@@ -412,13 +412,13 @@ void TBlock::moveDown(Board *board) {
         for (int i =0; i<4; ++i) {
             int coorX = cells[i]->getX();
             int coorY = cells[i]->getY();
-            cells[i]->Swap(&grid[coorX][coorY-1]);
+            cells[i]->Swap(&grid[coorX+1][coorY]);
         }
     } else {
         for (int i =3; i>=0; --i) {
             int coorX = cells[i]->getX();
             int coorY = cells[i]->getY();
-            cells[i]->Swap(&grid[coorX][coorY-1]);
+            cells[i]->Swap(&grid[coorX+1][coorY]);
         }
     }
 }
